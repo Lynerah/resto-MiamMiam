cd4cd3e2b712d04d3a20064264597a77fc4164ae
Lynerah <celinedeboeck@gmail>
2021-12-16 17:29:19 +0100

    template general page

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const arrayCards = [
+   {
+      name: "Burger1",
+      description: "deliceuse trache de lard avec sa sauce bbq!",
+      img: "./img/food-photographer-david-fedulov-Fn6dPYtPUMc-unsplash.jpg"
+   },
+   {
+      name: "Burger2",
+      description: "deliceuse trache de lard avec sa sauce bbq!",
+      img: "./img/food-photographer-david-fedulov-Fn6dPYtPUMc-unsplash.jpg"
+      
+   },
+   {
+      name: "Burger3",
+      description: "deliceuse trache de lard avec sa sauce bbq!",
+      img: "./img/food-photographer-david-fedulov-Fn6dPYtPUMc-unsplash.jpg"
+   },
+   {
+      name: "Burger4",
+      description: "deliceuse trache de lard avec sa sauce bbq!",
+      img: "./img/food-photographer-david-fedulov-Fn6dPYtPUMc-unsplash.jpg"
+   },
+
+]
+
+for (let arrayCard of arrayCards){
+   console.log("mon burger" + arrayCard.name );
+}
